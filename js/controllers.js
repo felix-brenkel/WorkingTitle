ba0313e05f16c4de0f0ce599c429337cc34efc7e
felix-brenkel <felix.brenkel@njuce.com>
2014-04-15 16:57:25 +0200

    Added AngularJS Location Controller

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+function LocationController($scope) {
+    $scope.locations = [{
+        postCode: '73061',
+        city: 'Ebersbach'
+    }, {
+        postCode: '73776',
+        city: 'Altbach'
+    }];
+}
