963c186435b2312c4e27249e2de22359ebe6d240
felix-brenkel <felix.brenkel@njuce.com>
2014-04-17 14:04:02 +0200

    Using AngularJS Views, Routing, Controllers

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,38 @@
-function LocationController($scope) {
-    $scope.locations = [{
+/* global angular */
+
+var locationApp = angular.module('locationApp', ['ngRoute'])
+
+.factory('LocationFactory', function () {
+    var factory = {};
+    var customers = [{
         postCode: '73061',
         city: 'Ebersbach'
     }, {
         postCode: '73776',
         city: 'Altbach'
     }];
-}
+    factory.getCustomers = function () {
+        return customers;
+    };
+    return factory;
+})
+
+.controller('LocationController', function ($scope, LocationFactory) {
+    $scope.locations = LocationFactory.getCustomers();
+
+    $scope.addLocation = function () {
+        $scope.locations.push({
+            postCode: $scope.newLocation.postCode,
+            city: $scope.newLocation.city
+        });
+    };
+});
+
+
+locationApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'LocationController',
+            templateUrl: 'views/locations.html'
+        });
+});
